082297d37100a8054cb6115ee2c346a15c7aa290
smallccn <sdwangwei0922@126.com>
2025-03-18 14:00:28 +0000

    Add comment for stage 1 code

--- SFT_stage1.py
+++ SFT_stage1.py
@@ -27,6 +27,7 @@ class Train_dataset(torch.utils.data.Dataset):
     def __init__(self, config, tokenizer):
         self.config = config
         self.tokenizer = tokenizer
+        # sft的数据集是个list, list中的每一项内容是dict
         with open(config.data_path) as f:
             self.data = json.load(f)
         
@@ -50,34 +51,40 @@ class Train_dataset(torch.utils.data.Dataset):
         return self.data[index]
 
     def get_response(self,da):
+        # 格式化输出的内容，将cot和response与特殊字符拼接到一起。
         temp = '## Thinking\n\n{}\n\n## Final Response\n\n{}'
         return temp.format(da['Complex_CoT'],da['Response'])
 
 
     def get_prompt(self,da):
 
-        q = da['Question']
-        a = self.get_response(da)
+        q = da['Question']  # 问题
+        a = self.get_response(da)  # 带cot的response
         assert q is not None and a is not None, f'q:{q} a:{a}'
 
+        # 指令微调格式的输入+输出
         input =  self.template.render(messages=[{"role": "user", "content": q},{"role": "assistant", "content": a}],bos_token=self.tokenizer.bos_token,add_generation_prompt=False)
         input_ids = self.tokenizer.encode(input,add_special_tokens= False)
 
+        # 指令微调格式的输入
         query = self.template.render(messages=[{"role": "user", "content": q}],bos_token=self.tokenizer.bos_token,add_generation_prompt=True)
         query_ids = self.tokenizer.encode(query,add_special_tokens= False)
 
+        # 用-100 mask输入部分，减少loss计算的时候的计算量
         labels = [-100]*len(query_ids) + input_ids[len(query_ids):]
         assert len(labels) == len(input_ids)
         return {"input_ids": input_ids[-self.max_seq_len:], "labels": labels[-self.max_seq_len:]}        
 
     def collate_fn(self, batch):
-        data = [ self.get_prompt(da) for da in batch]
+        data = [ self.get_prompt(da) for da in batch] # token化+指令跟随+cot拼接+mask labels
         input_ids = [item["input_ids"] for item in data]
         labels = [item["labels"] for item in data]
         max_len = max(len(x) for x in input_ids)
         max_len = min(max_len,self.max_seq_len)
+        # 这里是pad eos token_id ？
         input_ids = [ item[:max_len] + [self.tokenizer.eos_token_id]*(max_len-len(item)) for item in input_ids]
         labels = [ item[:max_len] + [-100]*(max_len-len(item)) for item in labels]
+        # 打印前3条数据
         if self.debug < 3:
             print('input_ids',self.tokenizer.decode(input_ids[-1]))
             print('labels',self.tokenizer.decode([0 if x == -100 else x for x in labels[-1]]))
@@ -102,6 +109,9 @@ class SFTMetric:
     def __call__(self, logits, labels, loss):
         return self.update(logits, labels, loss)
 
+    # self.right = 累积预测正确的token数
+    # self.total 累积预测生成了多少token
+    # self.total_loss 累积到当前step到loss和
     def update(self, logits, labels, loss):
         self.n_step += 1
         with torch.no_grad():
@@ -111,6 +121,7 @@ class SFTMetric:
             self.total += (shift_labels != -100).sum().item()
             self.total_loss += loss.item()
 
+    # get得到的acc是总预测正确的token/总生成token， loss是总loss求了个平均。
     def get_metric(self, reset=True):
         dist.all_reduce(self.right, op=torch.distributed.ReduceOp.SUM)
         dist.all_reduce(self.total, op=torch.distributed.ReduceOp.SUM)
@@ -128,24 +139,29 @@ class SFTMetric:
 
 
 def train(args):
-
+    # 初始化accelerator
     accelerator = Accelerator(mixed_precision='bf16', gradient_accumulation_steps=args.gradient_accumulation_steps) 
 
+    # wandb offline模式，wandb的日志不会上传，需要分析可以在训练完成之后再wandb sync上传
     if accelerator.is_main_process:
         wandb.init(project = args.experiment_name, config=args, dir=args.log_dir, mode="offline")
     
     accelerator.print(f'args:\n{args}')
 
+    # 使用的是DP训练，train_bsz_per_gpu是每个gpu上的batch size， global bsz = per_gpu * gpu 数量 * gradient_accumulation_steps
     accelerator.state.deepspeed_plugin.deepspeed_config['train_micro_batch_size_per_gpu'] = args.train_bsz_per_gpu
     accelerator.state.deepspeed_plugin.deepspeed_config['train_batch_size'] = args.train_bsz_per_gpu*dist.get_world_size()*accelerator.gradient_accumulation_steps
 
+    # 初始化预训练的tokenizer和模型
     tokenizer = AutoTokenizer.from_pretrained(args.model_path, trust_remote_code=True)
 
     model = AutoModelForCausalLM.from_pretrained(args.model_path, trust_remote_code=True)
 
     # open gradient checkpointing
+    # 使能 gradient checkpointing, 这里用的是transformers里面定义的模型的使能方法
     model.gradient_checkpointing_enable()
 
+    # 优化器参数分组， bias 和LN的权重一组，其他一组
     no_decay = ["bias", "LayerNorm.weight"]
     optimizer_grouped_parameters = [
         {
@@ -158,23 +174,32 @@ def train(args):
         },
     ]
 
+    # 优化器用的是optimizer
     optimizer = torch.optim.AdamW(optimizer_grouped_parameters, lr=args.learning_rate)
 
+    # 初始化训练数据集
     train_dataset = Train_dataset(args, tokenizer)
+    # 加载数据集，这里开了shuffle 和 drop_last
     train_dataloader = DataLoader(train_dataset, batch_size=args.train_bsz_per_gpu, shuffle=True, drop_last=True, collate_fn=train_dataset.collate_fn)
-
+    # 计算总训练步数。数据集数量 * epochs // gradient_accumulation_steps // gpu数量
     num_training_steps = int(len(train_dataloader) * (args.n_epochs)) // accelerator.gradient_accumulation_steps // dist.get_world_size()
+    # 初始化lr scheduler
     lr_scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=int(args.warmup_rates * num_training_steps), num_training_steps=num_training_steps)
     accelerator.print(f'gradient_accumulation_steps:{accelerator.gradient_accumulation_steps} data_path:{args.data_path} lr:{args.learning_rate} num_training_steps:{num_training_steps}')
+    # prepare做了什么？
     model, optimizer, train_dataloader = accelerator.prepare(model, optimizer, train_dataloader)
 
     start_epoch = 0
     start_step = 0
     global_step = 0
 
+    # 记录训练的metric， 即acc和loss
     metric = SFTMetric(device=torch.cuda.current_device())
 
     def save_checkpoint(epoch, step, global_step):
+        # 保存ckpt
+        # 只保留最近的max_ckpts
+        # 保留model的state dict， tokenizer和源模型文件夹中非权重的其他文件
         save_dir = os.path.join(args.output_dir, f"checkpoint-{epoch}-{global_step}")
         if accelerator.is_main_process:
             checkpoint_files = os.listdir(args.output_dir)
@@ -203,7 +228,7 @@ def train(args):
                     shutil.copy(s, os.path.join(output_dir,item))
                 copy_files.append(item)
             print(f'huggingface model save in {output_dir}, copy file:{copy_files}')
-
+        # 开了zero3, 保存之前要把模型先unwrap回来
         if accelerator.state.deepspeed_plugin.zero_stage==3:
             unwrap_model = accelerator.unwrap_model(model)
             unwrap_model.save_pretrained(os.path.join(save_dir, f'tfmr'),is_main_process=accelerator.is_main_process,save_function=accelerator.save,state_dict=accelerator.get_state_dict(model))
@@ -212,6 +237,7 @@ def train(args):
         accelerator.save({"epoch": epoch, "step": step, "global_step": global_step}, os.path.join(save_dir, "training_state.pt"))
         accelerator.print(f'checkpoint checkpoint-{epoch}-{global_step} is saved...')
 
+    # 切换模型模式，准备开始训练
     accelerator.print(accelerator.deepspeed_config)
     model.train()
 
@@ -230,9 +256,13 @@ def train(args):
             output = model(input_ids=input_ids, labels=labels, return_dict=True,use_cache=False)
             loss = output.loss
 
+            # 把当前iter夏的loss 更新到metric中
+            # 获取到的acc是累积的acc， loss是全部平均的loss
             metric(output.logits, labels, loss)
             acc, train_loss = metric.get_metric()
+            # 执行反向
             accelerator.backward(loss)
+            # 更新权重，lr。
             if (global_step+1) % accelerator.gradient_accumulation_steps == 0:
                 optimizer.step()
                 lr_scheduler.step()
@@ -240,9 +270,11 @@ def train(args):
 
             global_step += 1
 
+            # 主进程打印日志
             if accelerator.is_main_process:
                 train_dataloader_iterator.set_postfix(epoch=epoch, current_step=batch_cnt, total_step=len(train_dataloader), skip=accelerator.optimizer_step_was_skipped, loss=round(train_loss, 3), acc=round(acc, 3), length=len(input_ids[0]), lr=lr_scheduler.get_last_lr()[0])
 
+            # 保存wandb的日志
             if global_step % 3 == 0 and accelerator.is_main_process:
                 wandb.log({
                     'skip': int(accelerator.optimizer_step_was_skipped),
@@ -250,7 +282,7 @@ def train(args):
                     'acc': acc,
                     'lr': lr_scheduler.get_last_lr()[0]
                 }, step=global_step)
-
+        # 每个epoch 保存一次ckpt
         accelerator.wait_for_everyone()
         save_checkpoint(epoch, batch_cnt, global_step)
 
